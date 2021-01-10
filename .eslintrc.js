675e4fc1d4fa427b2dedf09e06919fdb042213f8
Louie John Seno <senolouiejohn@gmail.com>
2021-01-10 14:11:01 +0800

    feat: init

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    parser: "@typescript-eslint/parser",
+    extends: [
+        "plugin:@typescript-eslint/recommended",
+        "prettier/@typescript-eslint",
+        "plugin:prettier/recommended",
+        "plugin:react/recommended",
+    ],
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: "module",
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    rules: {
+        "@typescript-eslint/explicit-function-return-type": "off",
+    },
+    overrides: [
+        {
+            files: ["*.ts", "*.tsx"],
+            rules: {
+                "@typescript-eslint/explicit-function-return-type": ["off"],
+            },
+        },
+    ],
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
+}
