49b0b97337d3eafc6ddad6a8231652c260803ada
Louie John Seno <senolouiejohn@gmail.com>
2021-01-17 17:32:34 +0800

    feat: init bulma framework

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react"
 import { Provider } from "react-redux"
-import "./App.css"
+import "./styles/App.sass"
+import Button from "react-bulma-components/lib/components/button"
 import { store } from "./boot"
 const App = () => (
     <Provider store={store}>
         <p>React ecommerce</p>
+        <Button color="primary">My Bulma button</Button>
     </Provider>
 )
 
