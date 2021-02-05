c86a29352b8717b978a59a0f0b5de2c8f8e51dba
Louie John Seno <senolouiejohn@gmail.com>
2021-02-05 21:28:49 +0800

    feat: login page

--- src/app/pages/authentication/Login.jsx
+++ src/app/pages/authentication/Login.jsx
@@ -1,33 +1,35 @@
 import React from "react"
+import "./authentication.modules.css"
 
 function AuthContent() {
     return (
-        <div className="columns is-centered">
-            <div className="column is-half">
-                <div className="card ">
+        <div className="hero-body columns is-centered is-fullheight">
+            <div className="column is-one-quarter ">
+                <div className="card card-wrapper has-text-centered">
+                    <p className="login-text is-size-3">Login</p>
                     <div className="field">
-                        <p className="control has-icons-left has-icons-right">
-                            <input className="input" type="email" placeholder="Email" />
-                            <span className="icon is-small is-left">
-                                <i className="fas fa-envelope"></i>
-                            </span>
-                            <span className="icon is-small is-right">
-                                <i className="fas fa-check"></i>
-                            </span>
-                        </p>
-                    </div>
-                    <div className="field">
-                        <p className="control has-icons-left">
-                            <input className="input" type="password" placeholder="Password" />
-                            <span className="icon is-small is-left">
-                                <i className="fas fa-lock"></i>
-                            </span>
+                        <p className="control">
+                            <input className="input is-primary is-normal" type="email" placeholder="Email" />
                         </p>
                     </div>
                     <div className="field">
                         <p className="control">
-                            <button className="button is-success">Login</button>
+                            <input className="input is-primary is-normal" type="password" placeholder="Password" />
                         </p>
+                    </div>
+                    <div className="field">
+                        <p className="control">
+                            <button className="button is-primary is-normal is-fullwidth">Submit</button>
+                        </p>
+                    </div>
+                    <div className="login-utils-wrapper">
+                        <a href="">Forgot Password?</a>
+                        <div className="columns is-1 sign-up">
+                            <div className="column">Not a member yet?</div>
+                            <div className="column">
+                                <a href="">Create Account</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
