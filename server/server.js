1df2966ab6ff7371d6a06ba2ef824b543c7c2241
Louie John Seno <senolouiejohn@gmail.com>
2021-02-12 22:01:44 +0800

    fix: port server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const path = require("path")
 const express = require("express")
 const app = express()
-const port = 3000
+const port = process.env.port || 3000
 const publicPath = path.join(__dirname, "..", "build")
 
 app.use(express.static(publicPath))
