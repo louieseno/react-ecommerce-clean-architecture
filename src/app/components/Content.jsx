c01d625f4342a7b1e315109e8c6b97487e20161f
Louie John Seno <senolouiejohn@gmail.com>
2021-02-13 23:30:32 +0800

    fix: invalid fragment usage

--- src/app/components/Content.jsx
+++ src/app/components/Content.jsx
@@ -1,15 +1,15 @@
-import React, { Fragment } from "react"
+import React from "react"
 import Header from "./Header"
 import Footer from "./Footer"
 import style from "./components.module.css"
 const { mainContent } = style
 var Content = function (WrappedComponent, includeHeader, includeFooter) {
     return (
-        <Fragment className={mainContent}>
+        <div className={mainContent}>
             {includeHeader && <Header></Header>}
             <WrappedComponent></WrappedComponent>
             {includeFooter && <Footer></Footer>}
-        </Fragment>
+        </div>
     )
 }
 export default Content
