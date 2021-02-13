2191aa3ecab3cdb7aae68a6f1fdf301123c30355
Louie John Seno <senolouiejohn@gmail.com>
2021-02-13 23:29:06 +0800

    feat: questionDropdown component hanlder

--- src/app/pages/details/FAQ.jsx
+++ src/app/pages/details/FAQ.jsx
@@ -4,60 +4,59 @@ import styles from "./details.module.css"
 const { detailHeader, infoImage } = styles
 
 function FAQContent() {
-    const [dropdownActive, setDropdownActive] = useState(false)
-    const handleClick = () => {
-        if (dropdownActive) {
-            setDropdownActive(false)
+    let dropdownStates = { order1: false, order2: false }
+    const [dropdownActive, setDropdownActive] = useState(dropdownStates)
+
+    const handleClick = (stateKey) => {
+        const previous = { ...dropdownActive }
+        if (!dropdownActive[stateKey]) {
+            previous[stateKey] = true
         } else {
-            setDropdownActive(true)
+            previous[stateKey] = false
         }
-        console.log(dropdownActive)
+        setDropdownActive(previous)
     }
+
+    const questionDropdown = (question, answer, stateKey) => {
+        return (
+            <div className={`dropdown ${dropdownActive[stateKey] ? "is-active" : ""}`}>
+                <div className="dropdown-trigger">
+                    <button
+                        className="button"
+                        aria-haspopup="true"
+                        aria-controls="dropdown-menu3"
+                        onClick={() => handleClick(stateKey)}
+                    >
+                        <span>{question}</span>
+                        <span className="icon is-small">
+                            <i className="fas fa-angle-down" aria-hidden="true"></i>
+                        </span>
+                    </button>
+                </div>
+                <div className="dropdown-menu" id="dropdown-menu3" role="menu">
+                    <div className="dropdown-content">
+                        <p className="dropdown-item">{answer}</p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     return (
         <div className={`columns is-centered hero-body ${infoImage}`}>
             <div className={`column is-two-fifths has-text-centered `}>
                 <p className={detailHeader}>{"FAQ"}</p>
-                <div className={`dropdown ${dropdownActive ? "is-active" : ""}`}>
-                    <div className="dropdown-trigger">
-                        <button
-                            className="button"
-                            aria-haspopup="true"
-                            aria-controls="dropdown-menu3"
-                            onClick={() => handleClick()}
-                        >
-                            <span>Click me</span>
-                            <span className="icon is-small">
-                                <i className="fas fa-angle-down" aria-hidden="true"></i>
-                            </span>
-                        </button>
-                    </div>
-                    <div className="dropdown-menu" id="dropdown-menu3" role="menu">
-                        <div className="dropdown-content">
-                            <a href="#" className="dropdown-item">
-                                Overview
-                            </a>
-                            <a href="#" className="dropdown-item">
-                                Modifiers
-                            </a>
-                            <a href="#" className="dropdown-item">
-                                Grid
-                            </a>
-                            <a href="#" className="dropdown-item">
-                                Form
-                            </a>
-                            <a href="#" className="dropdown-item">
-                                Elements
-                            </a>
-                            <a href="#" className="dropdown-item">
-                                Components
-                            </a>
-                            <a href="#" className="dropdown-item">
-                                Layout
-                            </a>
-                            <hr className="dropdown-divider" />
-                        </div>
-                    </div>
-                </div>
+                Order Status and / or Change
+                {questionDropdown(
+                    "How do I check the status of my order?",
+                    "You can also check the status of your order by logging into your We Wear Where account and reviewing your order history section by clicking ‘Account’ > ‘View my orders’. If you do not have a We Wear Where account or you are not logged on, please log into your We Wear Where membership account.",
+                    "order1",
+                )}
+                {questionDropdown(
+                    "How can I check my order history?",
+                    "You can check the order history by logging into your We Wear Where account and reviewing the order history section by clicking ‘Account’ > ‘View my orders’. If you do not see your order history in your account, please get in touch with our friendly Customer Support Team using any available channels below.",
+                    "order2",
+                )}
             </div>
         </div>
     )
