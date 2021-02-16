f0c9d4c103241b5ae65981c32cdbbeb045f798e0
Louie John Seno <senolouiejohn@gmail.com>
2021-02-16 22:21:53 +0800

    fix: ignore config

--- src/boot.ts
+++ src/boot.ts
@@ -6,19 +6,8 @@ import firebase from "firebase/app"
 import { applyMiddleware, combineReducers, createStore } from "redux"
 import thunk from "redux-thunk"
 import faq from "app/redux/faq/faq.reducers"
+import firebaseConfig from "firebase.config"
 
-let _firebaseConfig = {}
-
-_firebaseConfig = {
-    apiKey: "AIzaSyDdjV7xYh_-U3rssXg1bYjyn3yB5viMmkU",
-    authDomain: "we-wear-where.firebaseapp.com",
-    projectId: "we-wear-where",
-    storageBucket: "we-wear-where.appspot.com",
-    messagingSenderId: "640068816961",
-    appId: "1:640068816961:web:6310dcc7bb8b808f9c76c0",
-    measurementId: "G-YWZMB3966X",
-}
-
-firebase.initializeApp(_firebaseConfig)
+firebase.initializeApp(firebaseConfig)
 
 export const store = createStore(combineReducers({ faq }), applyMiddleware(thunk))
