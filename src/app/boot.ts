02ad35e25207e261feb97bd2aa792a9097a217fd
Louie John Seno <senolouiejohn@gmail.com>
2021-02-17 21:59:02 +0800

    fix: firebase config

--- src/app/boot.ts
+++ src/app/boot.ts
@@ -6,20 +6,14 @@ import firebase from "firebase/app"
 import { applyMiddleware, combineReducers, createStore } from "redux"
 import thunk from "redux-thunk"
 import faq from "app/redux/faq/faq.reducers"
-import { config } from "./config"
-const _checkEnvironment = () => {
-    return process.env.NODE_ENV === "production"
+let firebaseConfig = {}
+if (process.env.NODE_ENV === "production") {
+    firebaseConfig = require("app/config/production")
+} else {
+    console.log("dev")
+    firebaseConfig = require("app/config/development")
 }
-const firebaseConfig = {
-    apiKey: _checkEnvironment() ? process.env.apiKey : config.apiKey,
-    authDomain: _checkEnvironment() ? process.env.authDomain : config.authDomain,
-    projectId: _checkEnvironment() ? process.env.projectId : config.projectId,
-    storageBucket: _checkEnvironment() ? process.env.storageBucket : config.storageBucket,
-    messagingSenderId: _checkEnvironment() ? process.env.messagingSenderId : config.messagingSenderId,
-    appId: _checkEnvironment() ? process.env.appId : config.appId,
-    measurementId: _checkEnvironment() ? process.env.measurementIdq : config.measurementId,
-}
-
+console.log(firebaseConfig)
 firebase.initializeApp(firebaseConfig)
 
 export const store = createStore(combineReducers({ faq }), applyMiddleware(thunk))
---
+++ src/app/config/production.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementIdq,
+}
