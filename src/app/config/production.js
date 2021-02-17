44b29e1ca9bed83829ea9b7b5a1aa0bc151197d3
Louie John Seno <senolouiejohn@gmail.com>
2021-02-17 22:15:50 +0800

    fix: environement variables

--- src/app/config/production.js
+++ src/app/config/production.js
@@ -1,9 +1,9 @@
 module.exports = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId,
-    measurementId: process.env.measurementId,
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID,
 }
