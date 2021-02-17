52238f7e746908871555b09705ac1b8ef4284df6
Louie John Seno <senolouiejohn@gmail.com>
2021-02-17 21:38:50 +0800

    fix: firebase config

--- src/app/boot.ts
+++ src/app/boot.ts
@@ -6,7 +6,19 @@ import firebase from "firebase/app"
 import { applyMiddleware, combineReducers, createStore } from "redux"
 import thunk from "redux-thunk"
 import faq from "app/redux/faq/faq.reducers"
-import { firebaseConfig } from "./config"
+import { config } from "./config"
+const _checkEnvironment = () => {
+    return process.env.NODE_ENV === "production"
+}
+const firebaseConfig = {
+    apiKey: _checkEnvironment() ? process.env.apiKey : config.apiKey,
+    authDomain: _checkEnvironment() ? process.env.authDomain : config.authDomain,
+    projectId: _checkEnvironment() ? process.env.projectId : config.projectId,
+    storageBucket: _checkEnvironment() ? process.env.storageBucket : config.storageBucket,
+    messagingSenderId: _checkEnvironment() ? process.env.messagingSenderId : config.messagingSenderId,
+    appId: _checkEnvironment() ? process.env.appId : config.appId,
+    measurementId: _checkEnvironment() ? process.env.measurementIdq : config.measurementId,
+}
 
 firebase.initializeApp(firebaseConfig)
 
