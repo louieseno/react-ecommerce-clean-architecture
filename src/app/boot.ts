4dd6894d5f0ec3f6222df4dabd9a715b236ebb57
Louie John Seno <senolouiejohn@gmail.com>
2021-02-18 20:19:38 +0800

    fix: remove prints

--- src/app/boot.ts
+++ src/app/boot.ts
@@ -8,12 +8,10 @@ import thunk from "redux-thunk"
 import faq from "app/redux/faq/faq.reducers"
 let firebaseConfig = {}
 if (process.env.NODE_ENV === "production") {
-    console.log("production")
     firebaseConfig = require("app/config/production")
 } else {
     firebaseConfig = require("app/config/development")
 }
-console.log(firebaseConfig)
 firebase.initializeApp(firebaseConfig)
 
 export const store = createStore(combineReducers({ faq }), applyMiddleware(thunk))
