ec4c304ed53b36963d346d30ba300635a75a91fe
Louie John Seno <senolouiejohn@gmail.com>
2021-02-20 20:31:16 +0800

    feat: navigate back faq menu

--- src/app/pages/details/FAQDetails.jsx
+++ src/app/pages/details/FAQDetails.jsx
@@ -1,19 +1,28 @@
-import React from "react"
+import React, { Fragment } from "react"
 import { useLocation } from "react-router-dom"
 import Content from "app/components/Content"
 import styles from "./details.module.css"
-const { detailSubHeader, detailWrapper, infoImage } = styles
+const { detailSubHeader, detailWrapper, infoImage, faqNavigatorBlock } = styles
 function FAQDetailsContent() {
     const location = useLocation()
     const question = location.state.question
     const answer = location.state.answer
+    const category = location.state.category
     return (
-        <div className={`columns is-centered hero-body ${infoImage}`}>
-            <div className={`column is-half has-text-centered ${detailWrapper}`}>
-                <p className={detailSubHeader}>{question}</p>
-                <p>{answer}</p>
+        <Fragment>
+            <div className={`${infoImage}  hero-body`}>
+                <div className={faqNavigatorBlock}>
+                    <a href="/faq">{`${category} > `}</a>
+                    <a href="">{question}</a>
+                </div>
+                <div className="columns is-centered">
+                    <div className={`column is-half has-text-centered ${detailWrapper}`}>
+                        <p className={detailSubHeader}>{question}</p>
+                        <p>{answer}</p>
+                    </div>
+                </div>
             </div>
-        </div>
+        </Fragment>
     )
 }
 const FAQDetails = () => {
